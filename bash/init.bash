dee185f117eca9cbe8840053444a0c8d5446f156
PowerShellHERO <62327737+PowerShellHERO@users.noreply.github.com>
2022-10-20 01:51:47 +0900

    add init.bash

---
+++ bash/init.bash
@@ -0,0 +1,16 @@
+
+bind '"\C-k": previous-history'
+bind '"\C-h": backward-char'
+bind '"\C-d": backward-delete-char'
+bind '"\C-l": forward-char'
+bind '"\C-g": delete-char'
+bind '"\C-c": yank'
+bind '"\C-e": beginning-of-line'
+bind '"\C-u": end-of-line'
+bind '"\C-z": undo'
+
+bind '"\C-k": previous-history'
+bind '"\C-k": previous-history'
+bind '"\C-k": previous-history'
+
+
