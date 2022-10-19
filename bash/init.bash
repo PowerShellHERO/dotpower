38e9b30c29479a181d2b03e9d01628c3052cddbb
PowerShellHERO <62327737+PowerShellHERO@users.noreply.github.com>
2022-10-20 02:09:08 +0900

    無駄を削除

--- bash/init.bash
+++ bash/init.bash
@@ -6,11 +6,8 @@ bind '"\C-l": forward-char'
 bind '"\C-g": delete-char'
 bind '"\C-c": yank'
 bind '"\C-e": beginning-of-line'
-bind '"\C-u": end-of-line'
+bind '"\C-u": end-of-line'   #設定不可能？
 bind '"\C-z": undo'
 
-bind '"\C-k": previous-history'
-bind '"\C-k": previous-history'
-bind '"\C-k": previous-history'
 
 
